b671ecbc393e921faf30f00fcafb4d913b0104c6
Essi Jukkala <essi.jukkala@aalto.fi>
2016-10-27 15:19:38 +0300

    fix jump

--- kuplajesse/Assets/Scripts/playerControl.cs
+++ kuplajesse/Assets/Scripts/playerControl.cs
@@ -38,12 +38,15 @@ public class playerControl : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		float h = Input.GetAxis("Horizontal");
+
 		if (grounded && Input.GetButtonDown ("Jump")) {
 			audio.Play ();
 			anim.SetBool ("Jump", true);
 			anim.SetBool ("Grounded", false);
 
-			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0f, jumpForce));
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, jumpForce));
+
 			grounded = false;
 		}
 		anim.SetFloat ("Velocity", GetComponent<Rigidbody2D> ().velocity.y);
@@ -70,7 +73,6 @@ public class playerControl : MonoBehaviour {
 		Physics2D.IgnoreLayerCollision (8, 10, GetComponent<Rigidbody2D> ().velocity.y > 0);
 		Physics2D.IgnoreLayerCollision (8, 9, killed);
 
-
         if (GetComponent<Rigidbody2D>().position.y < -6)
             GetComponent<Rigidbody2D>().gameObject.transform.position = new Vector2(GetComponent<Rigidbody2D>().position.x, 6);
 
@@ -78,13 +80,13 @@ public class playerControl : MonoBehaviour {
 		anim.SetFloat("Speed", Mathf.Abs(h));
 
 		if (h * GetComponent<Rigidbody2D> ().velocity.x < maxSpeed){
-			if (GetComponent<Rigidbody2D> ().velocity.y == 0)
+			//if (GetComponent<Rigidbody2D> ().velocity.y == 0)
 				GetComponent<Rigidbody2D> ().AddForce (Vector2.right * h * moveForce);
-			else
-				GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
+			//else
+			//	GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
 			}
         if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) >= maxSpeed)
-			if (GetComponent<Rigidbody2D> ().velocity.y == 0)
+			//if (GetComponent<Rigidbody2D> ().velocity.y == 0)
             	GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
         if (h > 0 && !facing) 
