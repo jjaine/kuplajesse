64167e07f2b82e0831e46a6484a4623aded19c22
Essi Jukkala <essi.jukkala@aalto.fi>
2017-06-07 18:10:57 +0300

    added start screen

---
+++ kuplajesse/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class LoadLevel : MonoBehaviour
+{
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Return)){
+			NextLevel();
+		}
+	}
+
+	public void NextLevel()
+	{
+        int level = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(level + 1);
+	}
+
+	public void ReloadLevel()
+	{
+		int level = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(level);
+	}
+}
